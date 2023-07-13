9dd8ae419254520d5d5c39b8fc182f1c27473b34
Mariane <a.marianesantana@gmail.com>
2023-07-12 23:40:03 -0300

    setting up hilt and di config

--- app/src/main/java/com/example/notesapp/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/feature_note/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.notesapp.feature_note.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,9 +11,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.notesapp.feature_note.presentation.add_edit_note.AddEditNoteScreen
+import com.example.notesapp.feature_note.presentation.notes.components.NotesScreen
+import com.example.notesapp.feature_note.presentation.util.Screen
 import com.example.notesapp.ui.theme.NotesAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +35,28 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Mariane Santana")
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = Screen.NotesScreen.route){
+                        composable(route = Screen.NotesScreen.route){
+                            NotesScreen(navController = navController)
+                        }
+                        composable(route = Screen.AddEditScreen
+                            .route + "?noteId={noteId}&noteColor={noteColor}",
+                        arguments = listOf(
+                            navArgument(name = "noteId") {
+                                type = NavType.IntType
+                                defaultValue = -1
+                            },
+                            navArgument(name = "noteColor") {
+                                type = NavType.IntType
+                                defaultValue = -1
+                            },
+                        )
+                        ){
+                            val color = it.arguments?.getInt("noteColor")?: -1
+                            AddEditNoteScreen(navController = navController, noteColor = color)
+                        }
+                    }
                 }
             }
         }
